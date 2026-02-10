6c1544b67e056719da26ade270bd36dfbf509a03
Chamika <infor.chamika@gmail.com>
2026-02-10 23:41:41 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,22 +2,26 @@ const mineflayer = require('mineflayer')
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: '88.198.2.26:33571', 
-        port: 33571,
-        username: 'Chiki_admin', 
+        host: '88.198.2.26', // මෙතැනට අංක ටික විතරක් දෙන්න
+        port: 33571,         // මෙතැනට තිත් දෙක නැතුව port එක විතරක් දෙන්න
+        username: 'Chiki_admin',
         version: '1.21'
     })
 
     bot.on('spawn', () => {
-        console.log('Bot joined! Logging in...')
-        bot.chat('/login Adminchiki') 
+        console.log('Bot joined the server!')
+        // සර්වර් එකට ආපු ගමන් Register හෝ Login වෙන්න
+        bot.chat('/register Chiki123 Chiki123') 
+        bot.chat('/login Chiki123')
     })
 
+    // සර්වර් එකෙන් Kick වුණොත් තත්පර 5කින් ආයෙත් Join වෙන්න
     bot.on('end', () => {
+        console.log('Disconnected. Reconnecting...')
         setTimeout(createBot, 5000)
     })
 
-    bot.on('error', err => console.log(err))
+    bot.on('error', err => console.log('Error:', err))
 }
 
 createBot()
